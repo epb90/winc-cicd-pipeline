fbab1dd08c266bdb41e5498c0c79dda5cb8a292e
epb90 <107769114+epb90@users.noreply.github.com>
2024-03-15 15:18:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from flask import Flask, redirect, render_template, request, session, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html', title="Index")
+def hello():
+    return 'Hello, world!'
