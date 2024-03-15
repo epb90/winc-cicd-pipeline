9f826c17e06627a72eaf945e6c11dda5609a77cd
rikwritescode <epbakker90@gmail.com>
2024-03-15 15:06:56 +0100

    second commit

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
 
-@pp.route('/')
+@app.route('/')
 def index():
     return render_template('index.html', title="Index")
